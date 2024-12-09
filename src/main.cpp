e4021a838556ef849e1c05ca50b060106cd00eee
rtarun9 <rtarun0913@gmail.com>
2024-12-09 08:24:39 +0530

    [WIP] Basic render loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <stdint.h>
 #include <stdio.h>
 
+#include <vector>
+
 typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
@@ -28,6 +30,24 @@ typedef double f64;
         int *ptr = NULL;                                                                                               \
         *ptr = 0;                                                                                                      \
     }
+
+struct frame_data_t
+{
+    VkCommandPool command_pool;
+    VkCommandBuffer command_buffer;
+
+    // Used to let the CPU know when this frame's GPU side rendering has been completed.
+    VkFence render_fence;
+
+    // Used to let the GPU know when a swapchain image has been retrieved.
+    VkSemaphore swapchain_semaphore;
+
+    // Used to let the GPU know when to present the swapchain image (i.e only after rendering on the image has been
+    // completed).
+    VkSemaphore render_semaphore;
+};
+
+#define FRAME_OVERLAP (u32)2
 
 int main(int argc, char *argv[])
 {
@@ -101,6 +121,71 @@ int main(int argc, char *argv[])
     // Get the VkDevice handle used in the rest of a vulkan application
     device = vkb_device.device;
     physical_device = vkb_physical_device.physical_device;
+    // Swapchain related objects and init code.
+    VkSwapchainKHR swapchain = {};
+    VkFormat swapchain_image_format = VK_FORMAT_R8G8B8A8_UNORM;
+    std::vector<VkImage> swapchain_images = {};
+    std::vector<VkImageView> swapchain_image_views = {};
+    VkExtent2D swapchain_extent = {};
+
+    VkSurfaceFormatKHR surface_format = {};
+    surface_format.format = swapchain_image_format;
+    surface_format.colorSpace = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR;
+
+    vkb::SwapchainBuilder vkb_swapchain_builder(physical_device, device, surface);
+    vkb::Swapchain vkb_swapchain = vkb_swapchain_builder.set_desired_format(surface_format)
+                                       .set_desired_present_mode(VK_PRESENT_MODE_FIFO_KHR)
+                                       .add_image_usage_flags(VK_IMAGE_USAGE_TRANSFER_DST_BIT)
+                                       .set_desired_extent(window_extent.width, window_extent.height)
+                                       .build()
+                                       .value();
+
+    swapchain_extent = vkb_swapchain.extent;
+    swapchain = vkb_swapchain.swapchain;
+    swapchain_images = vkb_swapchain.get_images().value();
+    swapchain_image_views = vkb_swapchain.get_image_views().value();
+
+    VkQueue graphics_queue = {};
+    u32 graphics_queue_family = 0;
+
+    frame_data_t frame_data[FRAME_OVERLAP];
+
+    graphics_queue = vkb_device.get_queue(vkb::QueueType::graphics).value();
+    graphics_queue_family = vkb_device.get_queue_index(vkb::QueueType::graphics).value();
+
+    // Create the command pool and buffer.
+    VkCommandPoolCreateInfo command_pool_create_info = {};
+    command_pool_create_info.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+    command_pool_create_info.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT;
+    command_pool_create_info.queueFamilyIndex = graphics_queue_family;
+
+    for (i32 i = 0; i < FRAME_OVERLAP; i++)
+    {
+        VK_CHECK(vkCreateCommandPool(device, &command_pool_create_info, NULL, &frame_data[i].command_pool));
+
+        // Now that command pool is created, allocate command buffers from it.
+        VkCommandBufferAllocateInfo command_buffer_allocate_info = {};
+        command_buffer_allocate_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+        command_buffer_allocate_info.commandBufferCount = 1;
+        command_buffer_allocate_info.commandPool = frame_data[i].command_pool;
+        command_buffer_allocate_info.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+
+        VK_CHECK(vkAllocateCommandBuffers(device, &command_buffer_allocate_info, &frame_data[i].command_buffer));
+
+        // Create sync primitives.
+        VkFenceCreateInfo fence_create_info = {};
+        fence_create_info.flags = VK_FENCE_CREATE_SIGNALED_BIT;
+        fence_create_info.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
+        fence_create_info.pNext = NULL;
+
+        VK_CHECK(vkCreateFence(device, &fence_create_info, NULL, &frame_data[i].render_fence));
+
+        VkSemaphoreCreateInfo semaphore_create_info = {};
+        semaphore_create_info.sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO;
+
+        VK_CHECK(vkCreateSemaphore(device, &semaphore_create_info, NULL, &frame_data[i].render_semaphore));
+        VK_CHECK(vkCreateSemaphore(device, &semaphore_create_info, NULL, &frame_data[i].swapchain_semaphore));
+    }
 
     i64 frame_number = 0;
 
@@ -121,7 +206,119 @@ int main(int argc, char *argv[])
                 quit = true;
             }
         }
+
+        // Main render loop.
+        {
+            frame_data_t *current_frame_data = &frame_data[frame_number % FRAME_OVERLAP];
+
+            VK_CHECK(vkWaitForFences(device, 1, &(current_frame_data->render_fence), true, 1e9));
+            VK_CHECK(vkResetFences(device, 1, &(current_frame_data->render_fence)));
+
+            // Request the swapchain for a image.
+            u32 swapchain_image_index = 0;
+            VK_CHECK(vkAcquireNextImageKHR(device, swapchain, 1e9, current_frame_data->swapchain_semaphore, NULL,
+                                           &swapchain_image_index));
+
+            VkCommandBuffer cmd = current_frame_data->command_buffer;
+
+            VK_CHECK(vkResetCommandBuffer(cmd, 0));
+
+            VkCommandBufferBeginInfo command_buffer_begin_info = {};
+            command_buffer_begin_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+            command_buffer_begin_info.pInheritanceInfo = NULL;
+            command_buffer_begin_info.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
+
+            VK_CHECK(vkBeginCommandBuffer(cmd, &command_buffer_begin_info));
+
+            VkImageSubresourceRange subresource_range = {};
+            subresource_range.aspectMask = VkImageAspectFlagBits::VK_IMAGE_ASPECT_COLOR_BIT;
+            subresource_range.baseMipLevel = 1;
+            subresource_range.levelCount = 1;
+            subresource_range.baseArrayLayer = 1;
+            subresource_range.layerCount = 1;
+
+            // Transition the swapchain image into a layout where we can draw into it.
+            {
+                VkImageMemoryBarrier2 image_memory_barrier = {};
+                image_memory_barrier.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER_2;
+                image_memory_barrier.srcStageMask = VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT;
+                image_memory_barrier.srcAccessMask = VK_ACCESS_2_MEMORY_WRITE_BIT;
+                image_memory_barrier.dstStageMask = VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT;
+                image_memory_barrier.dstAccessMask = VK_ACCESS_2_MEMORY_WRITE_BIT | VK_ACCESS_2_MEMORY_READ_BIT;
+                image_memory_barrier.oldLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+                image_memory_barrier.newLayout = VK_IMAGE_LAYOUT_GENERAL;
+
+                image_memory_barrier.image = swapchain_images[swapchain_image_index];
+
+                image_memory_barrier.subresourceRange = subresource_range;
+
+                VkDependencyInfo dependency_info = {};
+                dependency_info.sType = VK_STRUCTURE_TYPE_DEPENDENCY_INFO;
+                dependency_info.imageMemoryBarrierCount = 1;
+                dependency_info.pImageMemoryBarriers = &image_memory_barrier;
+
+                vkCmdPipelineBarrier2(cmd, &dependency_info);
+            }
+
+            VkClearColorValue clear_color = {};
+            clear_color.float32[2] = sinf(frame_number / 120.0f);
+
+            vkCmdClearColorImage(cmd, swapchain_images[swapchain_image_index], VK_IMAGE_LAYOUT_GENERAL, &clear_color, 1,
+                                 &subresource_range);
+
+            // Transition the swapchain image back to a format that can be presented.
+            {
+                VkImageMemoryBarrier2 image_memory_barrier = {};
+                image_memory_barrier.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER_2;
+                image_memory_barrier.srcStageMask = VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT;
+                image_memory_barrier.srcAccessMask = VK_ACCESS_2_MEMORY_WRITE_BIT;
+                image_memory_barrier.dstStageMask = VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT;
+                image_memory_barrier.dstAccessMask = VK_ACCESS_2_MEMORY_WRITE_BIT | VK_ACCESS_2_MEMORY_READ_BIT;
+                image_memory_barrier.oldLayout = VK_IMAGE_LAYOUT_GENERAL;
+                image_memory_barrier.newLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+                image_memory_barrier.image = swapchain_images[swapchain_image_index];
+
+                image_memory_barrier.subresourceRange = subresource_range;
+
+                VkDependencyInfo dependency_info = {};
+                dependency_info.sType = VK_STRUCTURE_TYPE_DEPENDENCY_INFO;
+                dependency_info.imageMemoryBarrierCount = 1;
+                dependency_info.pImageMemoryBarriers = &image_memory_barrier;
+
+                vkCmdPipelineBarrier2(cmd, &dependency_info);
+            }
+
+            VK_CHECK(vkEndCommandBuffer(cmd));
+        }
+        ++frame_number;
     }
+
+    // Wait for all gpu operations to be completed.
+    vkDeviceWaitIdle(device);
+
+    for (i32 i = 0; i < FRAME_OVERLAP; i++)
+    {
+        vkDestroyCommandPool(device, frame_data[i].command_pool, NULL);
+
+        vkDestroySemaphore(device, frame_data[i].render_semaphore, NULL);
+        vkDestroySemaphore(device, frame_data[i].swapchain_semaphore, NULL);
+
+        vkDestroyFence(device, frame_data[i].render_fence, NULL);
+    }
+
+    vkDestroySwapchainKHR(device, swapchain, NULL);
+    for (i32 i = 0; i < swapchain_image_views.size(); i++)
+    {
+        vkDestroyImageView(device, swapchain_image_views[i], NULL);
+    }
+
+    vkDestroySurfaceKHR(instance, surface, NULL);
+
+    vkDestroyDevice(device, NULL);
+
+    vkb::destroy_debug_utils_messenger(instance, debug_messenger);
+    vkDestroyInstance(instance, NULL);
 
     SDL_Quit();
 }
